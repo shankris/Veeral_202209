4baf430caa16f036750807d605b177f21785d9c0
Shankar Krishnasamy <shan_krishnasamy@yahoo.co.in>
2022-09-14 14:18:56 +0530

    Product List Added

---
+++ js/products.js
@@ -0,0 +1,20 @@
+let http = new XMLHttpRequest();
+http.open('get', '../js/products.json', true);
+http.send();
+http.onload = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		let products = JSON.parse(this.responseText);
+		let output = "";
+		for (let item of products) {
+			output += `
+	            <div class="product">
+	               <img src="${item.image}" alt="${item.description}" class="productListImg">
+	               <p class="title">${item.title}</p>
+	               <p class="description">${item.description}</p>
+	               <p class="more"><a href="">Find out more</a></p>
+	            </div>
+	         `;
+		}
+		document.querySelector(".products").innerHTML = output;
+	}
+}
